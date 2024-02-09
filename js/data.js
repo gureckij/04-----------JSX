004e7432dc08487f727a62c1d7cd47aafb26a4e4
Gureckij <gureckij@gmail.com>
2024-02-09 13:41:15 +0300

    Замена текста последней характеристике на "джакузи"

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const cardData = {
     description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
     img: 'img/photo-card-1.jpg',
     price: 15000,
-    features: ['2 каюты', '4 спальных места', 'аудиосистема'],
+    features: ['2 каюты', '4 спальных места', 'джакузи'],
 };
 
 window.cardData = cardData;
