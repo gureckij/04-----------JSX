bf4ab6a928d39f92337761d550347f2475305174
Gureckij <gureckij@gmail.com>
2024-02-09 13:38:12 +0300

    Замена отображения локальных переменных на отображение значения полей параметра cardData из файла data.js

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const cardData = {
+    name: 'Катер Meridian 401',
+    description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+    img: 'img/photo-card-1.jpg',
+    price: 15000,
+    features: ['2 каюты', '4 спальных места', 'аудиосистема'],
+};
+
+window.cardData = cardData;
