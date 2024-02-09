266a70153cd90533dcaeb40335957bba02cc637e
Gureckij <gureckij@gmail.com>
2024-02-09 17:38:12 +0300

    Изменение условия для параметра isTopSeller, чтобы он выделял карточки из списка

--- js/data.js
+++ js/data.js
@@ -1,6 +1,8 @@
+const topSellers = ['1', '3'];
+
 const cards = [
     {
-        id: 1,
+        id: '1',
         name: 'Катер Meridian 401',
         description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
         img: 'img/photo-card-1.jpg',
@@ -8,7 +10,7 @@ const cards = [
         features: ['2 каюты', '4 спальных места', 'аудиосистема, кондиционер'],
     },
     {
-        id: 2,
+        id: '2',
         name: 'Катер 60 Manhattan',
         description: 'Двухпалубное судно уровня VIP длинной 20 метров',
         img: 'img/photo-card-2.jpg',
