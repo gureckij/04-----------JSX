db5eae0cd923248dfcf943fadc07c1f56876361f
Gureckij <gureckij@gmail.com>
2024-02-09 14:36:05 +0300

    Передача правильных данных во второй компонент карточки

--- js/data.js
+++ js/data.js
@@ -1,9 +1,20 @@
-const cardData = {
-    name: 'Катер Meridian 401',
-    description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
-    img: 'img/photo-card-1.jpg',
-    price: 15000,
-    features: ['2 каюты', '4 спальных места', 'джакузи'],
-};
+const cards = [
+    {
+        id: 1,
+        name: 'Катер Meridian 401',
+        description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+        img: 'img/photo-card-1.jpg',
+        price: 15000,
+        features: ['2 каюты', '4 спальных места', 'аудиосистема, кондиционер'],
+    },
+    {
+        id: 2,
+        name: 'Катер 60 Manhattan',
+        description: 'Двухпалубное судно уровня VIP длинной 20 метров',
+        img: 'img/photo-card-2.jpg',
+        price: 25000,
+        features: ['6 кают', 'оборудованная кухня', 'джакузи'],
+    },
+];
 
-window.cardData = cardData;
+window.cards = cards;
